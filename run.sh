1f4dbb2838424e44ba4b8ba04af27c9b7fd3bb71
ubmt2 <106400798+ubmt2@users.noreply.github.com>
2026-06-13 22:18:34 +0000

    Исправил ошибку с монитьравнием томов, добавил две команды

--- run.sh
+++ run.sh
@@ -2,5 +2,15 @@
 
 
 case "$1" in 
- "build_generator" ) docker build -t generator_app ./generator
+    "build_generator") 
+    docker build -t generator-app ./generator
+ ;;
+    "create_local_data")
+    python3 ./generator/generate.py local_data
+;;
+    "run_generator")
+    docker run -v "$(pwd)/data:/data" generator-app
+;;
+
+ esac
  
